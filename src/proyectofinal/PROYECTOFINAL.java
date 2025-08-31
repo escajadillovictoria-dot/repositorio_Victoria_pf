57c3fe5cd1f6a0006aeea060fe8d217e51adf5d7
Victoria <Victoria@192.168.1.60>
2025-08-31 13:51:01 -0500

    carga inicial

---
+++ src/proyectofinal/PROYECTOFINAL.java
@@ -0,0 +1,34 @@
+package PROYECTOFINAL;
+
+import java.util.Scanner;
+
+public class PROYECTOFINAL {
+ 
+    public static void main (String[ ] arqs){
+      String cliente;
+        double c1,c2,c3,tc,pc;
+        Scanner lectura=new Scanner(System.in);
+                    
+       System.out.print("Ingresar nombre del cliente: ");
+       cliente=lectura.next();
+       
+       System.out.print("Ingresar la 1era compra realizada: ");
+       c1=lectura.nextDouble();
+       
+       System.out.print("Ingresar la 2da compra realizada: ");
+       c2=lectura.nextDouble();
+       
+       System.out.print("Ingresar la 3era compra realizada: ");
+       c3=lectura.nextDouble();
+       
+       tc=c1+c2+c3;
+       pc=tc/3;
+       
+       
+       System.out.println("La compra total es: "+tc);
+       System.out.println("El promedio de compra es: "+pc);
+     
+    }
+}
+  
+
